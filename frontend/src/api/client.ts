0209862d61c5efb98839ca4b282a1ca48120a183
DamjanT1988 <Damjan_t@hotmail.com>
2025-11-20 16:52:10 +0100

    chore: Docker

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "https://localhost:5091/api,"
-  baseURL: "https://localhost:7119/api",
+  //baseURL: "https://localhost:7119/api",
   //baseURL: "https://localhost:7284/api",
+  //baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://localhost:5091/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://localhost:5091/api",
 });
 
 export default api;
